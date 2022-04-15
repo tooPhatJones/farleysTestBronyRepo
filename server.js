c58f1bf1d12c43c95095c2241a9d317335fb6340
David Farley <davidfarley7171@gmail.com>
2022-04-14 18:05:31 -0700

    automated commit on Thu 14 Apr 2022 06:05:31 PM PDT on computer mrtophatjones

--- server.js
+++ server.js
@@ -1,50 +1,46 @@
-const express = require('express')
-var session = require('express-session');
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const httpPort = 9999;
+const httpsPort = 3001;
+app = express()
 
-var app = express();
+var key = fs.readFileSync(__dirname + '/rootCA.key');
+var cert = fs.readFileSync(__dirname + '/rootCA.pem');
 
+var credentials = {
+  key: key,
+  cert: cert
+};
+ 
 
-const oneDay = 1000 * 60 * 60 * 24;// creating 24 hours from milliseconds
-
-let sessionparams = {
-  secret: "thisismysecrctekeyfhrgfgrfrty84fwir767", // this is the only required parameter, in a production environment you would want this to be randomly generated refer to the docs for more
-  // the rest of these values do different things and there are a ton of options, but the explanations are all really meaty so Ill leave it for later.
-  saveUninitialized: true,
-  cookie: { maxAge: oneDay },
-  resave: false,
-  name: 'test'
-}
-
-app.use(session(sessionparams));
-
-app.get('/', function (req, res) {
-  console.log(req.session)
-
-  if (req.session.page_views) {
-    req.session.page_views++;
-
-    res.send("You visited this page " + req.session.page_views + " times, to decrement the counter visit <a href='http://localhost:"+port+"/-'>http://localhost:"+port+"/-</a>");
-    res.end()
-  } else {
-    req.session.page_views = 1;
-    res.send("Welcome to this page for the first time!");
-  }
+// redirect http requests to the https server.
+app.all('*', (req, res, next) => {
+    if (req.secure) {
+      return next();
+    } else {
+        console.log(`Redirecting to: https://${req.hostname}:${httpsPort}${req.url}`);
+        res.redirect(301, `https://${req.hostname}:${httpsPort}${req.url}`);
+    }
 });
 
-app.get('/-', function (req, res) {
-  if (req.session.page_views) {
-    req.session.page_views--;
-    res.send("the counter has been decremented, it is now " + req.session.page_views);
-  } else {
+//GET home route
+app.get('/', (req, res) => {
+   res.send('Hello World.');
+});
 
-    res.send("you have 0 page views, you might want to go back to <a href='http://localhost:"+port+"/'>http://localhost:"+port+"/</a>");
-  }
-}
-);
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+httpServer.listen(httpPort, () => {
+  console.log("Http server listing on port : " + httpPort)
+});
+
+httpsServer.listen(httpsPort, () => {
+  console.log("Https server listing on port : " + httpsPort)
+});
 
 
 //curl http://localhost:9999
